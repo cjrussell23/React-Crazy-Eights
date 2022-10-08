5893b205c091efca6dbb8b8a66f80e78c04112a8
cjrussell23 <cj.russell2396@gmail.com>
2022-10-08 01:24:50 -0600

    Lobby system with google auth

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,9 @@ import React from 'react'
 // import { doc, onSnapshot } from "firebase/firestore";
 
 export default function Game(props) {
-    const { lobbyId, leaveLobby, players, playerName } = props;
-
+    const { lobbyId, leaveLobby, players, user, readyPlayer } = props;
     return (
         <div>
-            <h1>Game: {playerName}</h1>
             <h2>Your lobby ID is: {lobbyId}</h2>
             <p>Give this to your friends so they can join!</p>
             <form>
@@ -17,9 +15,17 @@ export default function Game(props) {
             </form>
             <div>
                 <h2>Players</h2>
-                <ul>
+                <ul className='list-group list-group-horizontal'>
                     {players.map((player) => {
-                        return <li key={player.name}>{player.name}</li>
+                        return <li key={player.email}>
+                            <div className='card'>
+                                <img src={player.image} alt={`${player.name} profile`} className="rounded-circle" height="50px" width="50px" referrerPolicy="no-referrer"></img>
+                                <div className='card-body'>
+                                    <h5 className='card-title'>{player.name}</h5>
+                                    {user.email === player.email ? <button onClick={ () => {readyPlayer(player.ready)} }>{player.ready ? "Ready" : "Not Ready"}</button> : <p className='card-text'>{player.ready ? "Ready" : "Not Ready"}</p>}
+                                </div>
+                            </div>
+                        </li>
                     })}
                 </ul>
             </div>
